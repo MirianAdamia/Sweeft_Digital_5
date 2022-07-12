ffee8efc9ae0375c2f1aff9fca3b05a92c50f933
Mirian Adamia <m.adamia@Imedil.ge>
2022-07-12 18:22:10 +0400

    Add project files.

---
+++ Sweeft_Digital_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sweeft_Digital_5 { 
+     public class Program
+    {
+        static int countVariants(int stearsCount)
+        {
+            if (stearsCount <= 2)
+            {
+                return stearsCount;
+            }           
+            else
+            {
+                return countVariants(stearsCount - 2) + countVariants(stearsCount - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+           int result = countVariants(12);
+           Console.WriteLine(result);
+        }
+    }
+}
